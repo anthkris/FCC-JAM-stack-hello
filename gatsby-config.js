ed33ca472df7e355d42e6ced074fdbbb898f1570
anthkris <anthkris09@gmail.com>
2018-08-28 20:18:06 -0500

    feat: Initial commit of FCC JAM stack site

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Hello JAM Stack',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hello world! 👋</h1>
+    <p>My name is Kristin and I am currently embarking on a career change from learning experience design to web development. 🤯</p>
+    <p>I recently used Gatsby JS (v1) to update my personal portfolio and make it into a progressive web app.</p>
+    <p>Ahem... 👉 <a href="https://www.knanthony.com/">My portfolio</a></p>
+    <p>I'm currently looking for apprenticeships or junior-level positions. 🤙</p>
+  </div>
+)
+
+export default IndexPage
